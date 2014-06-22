0197ee0e0854bcb1c1c9cb7cde8c8bfabe357a05
toshi32tony3 <toshi32tony3@hotmail.com>
2014-06-22 18:10:50 +0900

    mod mklink.

--- mklink_dot.bat
+++ mklink_dot.bat
@@ -1,4 +1,3 @@
 REM mklink %HOME%\.vimrc %HOME%\dotfiles\.vimrc
 mklink %HOME%\.gvimrc %HOME%\dotfiles\.gvimrc
 mklink %HOME%\.vimshrc %HOME%\dotfiles\.vimshrc
-mklink %HOME%\..\Desktop\delete_vimfiles.bat %HOME%\batfiles\delete_vimfiles.bat
