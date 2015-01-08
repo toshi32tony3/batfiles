27605a06daa0261bd5abb41574d267c96a2eecd6
toshi32tony3 <toshi32tony3@hotmail.com>
2015-01-08 10:50:56 +0900

    remove memo

--- delete_vimfiles.bat
+++
@@ -1,3 +0,0 @@
- del %HOME%\vimfiles\backup
- del %HOME%\vimfiles\undo
- del %HOME%\vimfiles\view
--- mklink_dot.bat
+++ mklink_dot.bat
@@ -1,4 +1,6 @@
 REM mklink %HOME%\.vimrc %HOME%\dotfiles\.vimrc
 mklink %HOME%\.gvimrc %HOME%\dotfiles\.gvimrc
 mklink %HOME%\.vimshrc %HOME%\dotfiles\.vimshrc
+mklink %HOME%\.profile %HOME%\dotfiles\.profile
 pause
+
