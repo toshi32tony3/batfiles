4faa44ff6964e7dca926372f19e483559bdd1841
toshi32tony3 <toshi32tony3@hotmail.com>
2015-10-13 17:15:57 +0900

    mod delete_vimsiles.bat

--- delete_vimfiles.bat
+++ delete_vimfiles.bat
@@ -1,3 +1,12 @@
- del %HOME%\vimfiles\backup
- del %HOME%\vimfiles\undo
- del %HOME%\vimfiles\view
+@echo off
+echo Delete files for Vim.
+
+echo Delete %HOME%\vimfiles\backup
+del /Q %HOME%\vimfiles\backup
+
+echo Delete %HOME%\vimfiles\undo
+del /Q %HOME%\vimfiles\undo
+
+echo Delete %HOME%\vimfiles\view
+del /Q %HOME%\vimfiles\view
+pause
