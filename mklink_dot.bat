140fcdc7ed80aa620db9bffa9b3059face3b9aa1
toshi32tony3 <toshi32tony3@gmail.com>
2015-10-17 22:58:40 +0900

    uncomment

--- mklink_dot.bat
+++ mklink_dot.bat
@@ -1,4 +1,4 @@
-REM mklink %HOME%\.vimrc %HOME%\dotfiles\.vimrc
+mklink %HOME%\.vimrc %HOME%\dotfiles\.vimrc
 mklink %HOME%\.gvimrc %HOME%\dotfiles\.gvimrc
 mklink %HOME%\.vimshrc %HOME%\dotfiles\.vimshrc
 mklink %HOME%\.profile %HOME%\dotfiles\.profile
