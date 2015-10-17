dd0e50998323dee7dd4d20493164a31236466e5e
toshi32tony3 <toshi32tony3@gmail.com>
2015-10-17 23:04:18 +0900

    add settings for .gitconfig

--- mklink_dot.bat
+++ mklink_dot.bat
@@ -1,6 +1,7 @@
+mklink %HOME%\.gitconfig %HOME%\dotfiles\.gitconfig
+mklink %HOME%\.profile %HOME%\dotfiles\.profile
 mklink %HOME%\.vimrc %HOME%\dotfiles\.vimrc
 mklink %HOME%\.gvimrc %HOME%\dotfiles\.gvimrc
 mklink %HOME%\.vimshrc %HOME%\dotfiles\.vimshrc
-mklink %HOME%\.profile %HOME%\dotfiles\.profile
 pause
 
