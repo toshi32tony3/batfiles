1260bb0b3c8840f223bc92830480812b1a4222e6
toshi32tony3 <toshi32tony3@gmail.com>
2015-10-21 08:59:42 +0900

    add something_local.vim

--- mklink_dot.bat
+++ mklink_dot.bat
@@ -1,7 +1,9 @@
 mklink %HOME%\.gitconfig %HOME%\dotfiles\.gitconfig
 mklink %HOME%\.profile %HOME%\dotfiles\.profile
 mklink %HOME%\.vimrc %HOME%\dotfiles\.vimrc
+mklink %HOME%\vimrc_local.vim %VIM%\dotfiles\.vimrc
 mklink %HOME%\.gvimrc %HOME%\dotfiles\.gvimrc
+mklink %HOME%\gvimrc_local.vim %VIM%\dotfiles\.vimrc
 mklink %HOME%\.vimshrc %HOME%\dotfiles\.vimshrc
 pause
 
