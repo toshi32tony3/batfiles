cdb87dc75ddd89b349a5769cd533519530d1675a
toshi32tony3 <toshi32tony3@gmail.com>
2015-11-08 20:00:07 +0900

    add files for shiba

--- mklink_dot.bat
+++ mklink_dot.bat
@@ -1,9 +1,19 @@
+REM for git
 mklink %HOME%\.gitconfig %HOME%\dotfiles\.gitconfig
+
+REM for bash
 mklink %HOME%\.profile %HOME%\dotfiles\.profile
+
+REM for GVim
 mklink %HOME%\.vimrc %HOME%\dotfiles\.vimrc
 mklink %VIM%\vimrc_local.vim %HOME%\dotfiles\vimrc_local.vim
 mklink %HOME%\.gvimrc %HOME%\dotfiles\.gvimrc
 mklink %VIM%\gvimrc_local.vim %HOME%\dotfiles\gvimrc_local.vim
 mklink %HOME%\.vimshrc %HOME%\dotfiles\.vimshrc
+
+REM for Shiba
+mklink %APPDATA%\Shiba\config.yml %HOME%\batfiles\shiba-config.yml
+mklink %APPDATA%\Shiba\user.css %HOME%\batfiles\shiba-user.css
+
 pause
 
